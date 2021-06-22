849dd5fb8eafe2b1b8be4ecb187771049946432e
Mathias Peters <mathias.peters@senacor.com>
2021-06-22 08:57:31 +0200

    removed resource limits and switched to proper model

--- summarizer/model/summarize.py
+++ summarizer/model/summarize.py
@@ -1,14 +1,12 @@
-from transformers import LEDTokenizer, LEDForConditionalGeneration, LEDConfig, AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import LEDTokenizer, LEDForConditionalGeneration
 import torch
 
-#model = LEDForConditionalGeneration.from_pretrained('allenai/led-base-16384', cache_dir="cache")
-#tokenizer = LEDTokenizer.from_pretrained('allenai/led-base-16384', cache_dir="cache")
-model = AutoModelForSeq2SeqLM.from_pretrained("allenai/led-base-16384", cache_dir="cache")
-tokenizer = AutoTokenizer.from_pretrained("allenai/led-base-16384", cache_dir="cache")
+model = LEDForConditionalGeneration.from_pretrained('allenai/led-large-16384-arxiv')
+tokenizer = LEDTokenizer.from_pretrained('allenai/led-large-16384-arxiv')
 
 
 def predict(input_test: str, topic: str, summary_type: str):
-    max_length = 200 if summary_type == 'short' else 600
+    max_length = 180 if summary_type == 'short' else 400
     inputs = tokenizer.encode(input_test, return_tensors='pt')
     global_attention_mask = torch.zeros_like(inputs)
     global_attention_mask[:, 0] = 1
@@ -16,10 +14,11 @@ def predict(input_test: str, topic: str, summary_type: str):
     outputs = model.generate(
         inputs,
         global_attention_mask=global_attention_mask,
-        max_length=180,
+        max_length=max_length,
         min_length=50,
         length_penalty=1.2,
         num_beams=3,
+        no_repeat_ngram_size=3,
         early_stopping=True
     )
     return tokenizer.decode(outputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
