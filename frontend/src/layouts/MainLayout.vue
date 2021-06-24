1fd12eb2918ae492c7e598dc405474f43e640b82
Petr Roda <petr.roda@senacor.com>
2021-06-24 17:48:13 +0200

    Layout changes

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -16,12 +16,13 @@
         </q-toolbar-title>
         <q-space/>
         <div class="q-gutter-sm row items-center no-wrap ecb-font-regular">
-          <q-btn push color="secondary" text-color="white" label="Summarize" icon="menu_book" v-if="$q.screen.gt.sm" @click="openDialog = true">
+          <q-btn flat push color="secondary" text-color="white" label="Summarize" icon="menu_book"
+                 v-if="$q.screen.gt.sm" @click="openDialog = true">
             <q-tooltip>Run summarization</q-tooltip>
           </q-btn>
           <q-space/>
           <q-avatar>
-            <img src="../assets/logo_toolbar.jpg">
+            <img src="~assets/logo_toolbar.jpg">
           </q-avatar>
         </div>
       </q-toolbar>
@@ -48,121 +49,110 @@
       </q-list>
     </q-drawer>
 
-    <q-dialog v-model="openDialog" @hide="reset()" >
-      <q-card style="width: 80%; max-width: 80%; height: 70%; max-height: 70%">
-        <q-card-section horizontal>
-          <q-card-section class="col-3">
-            <div class="q-pa-md">
-              <div class="row q-gutter-x-md q-gutter-y-md">
-                <div class="col-grow q-gutter-x-md q-gutter-y-md">
-                  <q-select
-                    style="overflow: auto;"
-                    filled
-                    v-model="topicType"
-                    :options="optionsTopic"
-                    option-label="label"
-                    option-value="id"
-                    label="Type of topic">
-                    <template v-slot:prepend>
-                      <q-icon name="my_location"/>
-                    </template>
-                  </q-select>
-                </div>
-              </div>
-              <div class="row q-gutter-x-md q-gutter-y-md">
-                <div class="col-grow q-gutter-x-md q-gutter-y-md">
-                  <q-select
-                    style="overflow: auto;"
-                    filled
-                    v-model="summaryType"
-                    :options="optionsSummary"
-                    option-label="label"
-                    option-value="id"
-                    label="Size of summary">
-                    <template v-slot:prepend>
-                      <q-icon name="article"/>
-                    </template>
-                  </q-select>
-                </div>
-              </div>
-              <div class="row q-gutter-x-md q-gutter-y-md justify-center items-center content-center">
-                <div class="col q-gutter-x-md q-gutter-y-md justify-center items-center content-center">
-                  <q-uploader
-                    label="File to summarize"
-                    :factory="factoryFn"
-                    @uploaded="summarize"
-                    @failed="failed"
-                    @added="setFileSelected"
-                    @removed="unsetFileSelected"
-                    hide-upload-btn
-                    color="white"
-                    text-color="black"
-                    flat
-                    bordered
-                    accept=".txt"
-                    ref="uploader"
-                  />
-                </div>
-              </div>
-              <div class="row q-gutter-x-md q-gutter-y-md justify-center items-center content-center">
-                <div class="col q-gutter-x-md q-gutter-y-md justify-center items-center content-center">
-                  <q-btn :disabled="!isComplete" push color="primary" text-color="white" label="Summarize" icon="menu_book" @click="$refs.uploader.upload()">
-                    <q-tooltip>Run summarization</q-tooltip>
-                  </q-btn>
-                  <q-btn push color="secondary" text-color="white" label="Reset" @click="reset()">
-                    <q-tooltip>Reset</q-tooltip>
-                  </q-btn>
-                </div>
-              </div>
-            </div>
-          </q-card-section>
-          <q-separator/>
-          <q-card-section v-if="summaryVisible" class="col-6">
-            <div class="fit q-pa-md row">
-              <q-card flat style="overflow: auto;">
-                <q-card-section>
-                  <div class="text-h6">Summary</div>
-                </q-card-section>
+    <q-dialog v-model="openDialog" @hide="reset()" full-width class="">
+      <div class="row">
+        <div class="col-3">
+          <q-card style="overflow: auto; height: 30vw" square bordered>
+            <q-card-section>
+              <div class="text-h6 text-primary">Setup</div>
+            </q-card-section>
+            <q-card-section>
+              <q-select
+                style="overflow: auto;"
+                outlined
+                dense
+                v-model="topicType"
+                :options="optionsTopic"
+                option-label="label"
+                option-value="id"
+                label="Type of topic">
+                <template v-slot:prepend>
+                  <q-icon name="my_location"/>
+                </template>
+              </q-select>
+            </q-card-section>
+            <q-card-section>
+              <q-select
+                style="overflow: auto;"
+                outlined
+                dense
+                v-model="summaryType"
+                :options="optionsSummary"
+                option-label="label"
+                option-value="id"
+                label="Size of summary">
+                <template v-slot:prepend>
+                  <q-icon name="article"/>
+                </template>
+              </q-select>
+            </q-card-section>
+            <q-card-section>
+              <q-uploader
+                style="overflow: auto; width: inherit"
+                label="Textfile"
+                :factory="factoryFn"
+                @uploaded="summarize"
+                @failed="failed"
+                @added="setFileSelected"
+                @removed="unsetFileSelected"
+                hide-upload-btn
+                color="white"
+                text-color="black"
+                flat
+                bordered
+                accept=".txt"
+                ref="uploader"
+              />
+            </q-card-section>
+            <q-card-actions align="center">
+              <q-btn unelevated :disabled="!isComplete" color="primary" label="Summarize"
+                     @click="$refs.uploader.upload()">
+                <q-tooltip>Run summarization</q-tooltip>
+              </q-btn>
+              <q-btn unelevated color="primary" label="Reset" @click="reset()">
+                <q-tooltip>Reset</q-tooltip>
+              </q-btn>
+            </q-card-actions>
+          </q-card>
+        </div>
+        <div class="col-grow" style="background-color: white">
+          <q-card v-if="summaryVisible" flat style="overflow: auto; height: 30vw" square bordered>
+            <q-card-section>
+              <div class="text-h6 text-primary">Summary</div>
+            </q-card-section>
+            <q-card-actions>
+              <q-btn flat round color="primary" icon="save" @click="exportSummary"/>
+            </q-card-actions>
+            <q-card-section>
+              {{ summarization }}
+            </q-card-section>
+          </q-card>
+        </div>
+        <div class="col-2" style="background-color: white">
+          <q-card v-if="summaryVisible" flat style="overflow: auto; height: 30vw;" square bordered>
+            <q-card-section>
+              <div class="text-h6 text-primary">Rating</div>
+            </q-card-section>
 
-                <q-separator dark inset/>
-
-                <q-card-section>
-                  {{ summarization }}
-                </q-card-section>
-
-              </q-card>
-            </div>
-          </q-card-section>
-          <q-card-section v-if="summaryVisible">
-            <div class="fit q-pa-md row">
-              <q-card flat style="overflow: auto;">
-                <q-card-section>
-                  <div class="text-h6">Rate the summary</div>
-                </q-card-section>
-
-                <q-separator dark inset/>
-
-                <q-card-section>
-                  <q-rating
-                    v-model="ratingModel"
-                    size="2em"
-                    color="primary"
-                    icon="star_border"
-                    icon-selected="star"
-                  />
-                </q-card-section>
-                <q-card-section>
-                  By rating the quality of the summary you can improve
-                  the model quality by providing better training data
-                  for future model tuning.
-                  <br><br>
-                  This feature is not implemented yet.
-                </q-card-section>
-              </q-card>
-            </div>
-          </q-card-section>
-        </q-card-section>
-      </q-card>
+            <q-card-section>
+              <q-rating
+                v-model="ratingModel"
+                size="1.5em"
+                color="primary"
+                icon="star_border"
+                icon-selected="star"
+              />
+            </q-card-section>
+            <q-card-section>
+              By rating the quality of the summary you can improve
+              the model quality by providing better training data
+              for future model tuning.
+              <br><br>
+              This feature is not implemented yet.
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
     </q-dialog>
 
     <q-page-container>
@@ -177,9 +167,8 @@
 
 import EssentialLink from 'components/EssentialLink.vue'
 import {ref} from 'vue'
-import { useQuasar } from 'quasar'
+import {exportFile, useQuasar} from 'quasar'
 import { document_api } from 'boot/axios.js'
-
 
 const linksData = [
   {
@@ -205,11 +194,11 @@ export default {
       essentialLinks: linksData,
       optionsTopic: [
         {
-          id: 'asset_quality',
+          id: 'assetquality',
           label: 'Asset Quality'
         },
         {
-          id: 'climate_risk',
+          id: 'climaterisk',
           label: 'Climate Risk'
         },
         {
@@ -217,7 +206,7 @@ export default {
           label: 'Pandemic'
         },
         {
-          id: 'credit_lending',
+          id: 'creditlending',
           label: 'Credit Lending'
         }
       ],
@@ -236,10 +225,10 @@ export default {
       fileSelected: "",
       summarization: "...",
       openDialog: false,
-      summaryVisible: false
+      summaryVisible: true
     }
   },
-  setup () {
+  setup() {
     const $q = useQuasar()
     return {
       ratingModel: ref(0),
@@ -252,11 +241,29 @@ export default {
     }
   },
   computed: {
-    isComplete () {
+    isComplete() {
       return this.topicType != "" && this.summaryType != "" && this.fileSelected != "";
     }
   },
   methods: {
+    exportSummary() {
+      const $q = useQuasar()
+
+      const status = exportFile('summary.txt', this.summarization, {
+        encoding: 'UTF-8',
+        mimeType: 'text/plain;charset=UTF-8;'
+      })
+
+      if (status === true) {
+        // browser allowed it
+      } else {
+        // browser denied it
+        $q.notify({
+          type: 'negative',
+          message: 'Browser does not allow the export: Error code ' + status
+        })
+      }
+    },
     setFileSelected() {
       this.fileSelected = "true"
     },
@@ -270,7 +277,7 @@ export default {
       this.fileSelected = ""
       this.$refs.uploader.reset()
     },
-    factoryFn (files) {
+    factoryFn(files) {
       return {
         url: 'http://20.93.176.254:8000/upload',
         method: 'POST',
@@ -278,7 +285,7 @@ export default {
         formFields: [{name: 'topic', value: this.topicType.id}, {name: 'summary_type', value: this.summaryType.id}]
       }
     },
-    summarize ({ files, xhr }) {
+    summarize({files, xhr}) {
       this.summarization = JSON.parse(xhr.response).result
       this.summaryVisible = true
     }
