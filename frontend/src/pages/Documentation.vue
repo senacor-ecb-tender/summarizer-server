154d7ddc462c5978d55bdd75587f7bc6afc4d4e9
Petr Roda <petr.roda@senacor.com>
2021-06-26 11:25:25 +0200

    Landing page changes

--- frontend/src/pages/Documentation.vue
+++ frontend/src/pages/Documentation.vue
@@ -1,21 +1,18 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
     <div>
-      <div class="text-h2" style="opacity:.4">
+      <div class="text-h4" align="center">
         Hier entsteht die Dokumentation
       </div>
-
       <q-btn
         class="q-mt-xl"
         color="white"
         label="Go Home"
         no-caps
-        text-color="blue"
+        text-color="primary"
         to="/"
         unelevated
       />
     </div>
-  </div>
 </template>
 <script>
 export default {
