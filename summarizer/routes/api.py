48a1755831284a3fbaff1db7394d9237826552a8
Mathias Peters <mathias.peters@senacor.com>
2021-06-29 21:16:56 +0200

    refacted code to make it testable

--- summarizer/routes/api.py
+++ summarizer/routes/api.py
@@ -4,6 +4,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from ..model.summarize import predict
+from ..model.model_loader import ModelManager
 
 logger = logging.getLogger(__name__)
 api = APIRouter()
@@ -20,5 +21,5 @@ def index_html(request: Request):
 async def upload_file(topic=Form(...), summary_type=Form(...), file: UploadFile = File(...)):
     logger.info(f'Received upload for ${topic}, ${summary_type} and filename ${file.filename}')
     content = (await file.read()).decode('utf-8')
-    result = predict(content, topic, summary_type)
+    result = predict(content, topic, summary_type, ModelManager.instance())
     return {'result': result}
--- tests/routes/test_readiness.py
+++ tests/routes/test_readiness.py
@@ -1,7 +1,14 @@
 import pytest
+from summarizer.routes import readiness
+from fastapi import HTTPException
 
-@pytest.mark.skip
-def test_that_host_is_ready():
-    from summarizer.routes import readiness
 
+def test_that_host_is_ready(mocker, mock_model, mock_tokenizer):
+    mocker.patch.object(readiness.model_mgr, 'model', mock_model())
     assert readiness.get_readiness()['isReady'] == 'true'
+
+
+def test_that_host_is_not_ready_without_model():
+    with pytest.raises(HTTPException) as context:
+        readiness.get_readiness()
+        assert context.value.status_code == 503
