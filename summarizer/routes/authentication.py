f33c2c1c0bffd4b51ee747cac809ff27f3982399
Petr Roda <petr.roda@senacor.com>
2021-07-01 09:20:06 +0200

    new user and password

--- summarizer/routes/authentication.py
+++ summarizer/routes/authentication.py
@@ -10,8 +10,8 @@ security = HTTPBasic()
 
 
 def check_user(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "ecb_test_user")
-    correct_password = secrets.compare_digest(credentials.password, "B#@/:RX2uuS=`&29")
+    correct_username = secrets.compare_digest(credentials.username, "summarizer")
+    correct_password = secrets.compare_digest(credentials.password, "summarizer")
     if not (correct_username and correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
