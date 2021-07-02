5213d08999047946a50c9d32e1226d83a442c254
Kilian Kluge <kilian.kluge@gmail.com>
2021-07-02 11:29:11 +0200

    More properly prettify sentences, honor quotation marks, to not lowercase entire sentence

--- tests/model/test_post_process.py
+++ tests/model/test_post_process.py
@@ -4,8 +4,7 @@ from summarizer.model.post_process import prettify, process
 
 @pytest.mark.parametrize('ugly,pretty', [
     ('   this is a sentence with \n newlines and a small first letter in it.  ', 'This is a sentence with newlines and a small first letter in it.'),
-    ('  ', ''),
-    (None, None),
+    ('  ', '')
     ])
 def test_that_prettify_works(ugly, pretty):
     assert prettify(ugly) == pretty
@@ -19,3 +18,15 @@ def test_that_complete_sentences_remain():
 def test_that_only_whole_sentences_remain():
     text = "This is a text. It is quite ugly? And then it just"
     assert process(text) == ["This is a text.",  "It is quite ugly?"]
+
+
+def test_that_prettify_handles_quotation_marks():
+    text = "'how come you don't notice the quote?'"
+
+    assert prettify(text) == "'How come you don't notice the quote?'"
+
+
+def test_that_prettify_capitalizes_i():
+    text = "this is a text where i don't care."
+
+    assert prettify(text) == "This is a text where I don't care."
