acdd2aaae3d05c153194ba62b24bf2a813d4bc4d
Kilian Kluge <kilian.kluge@gmail.com>
2021-07-03 18:17:50 +0200

    Refactor model_loader, add test that ensures all config variables are passed

--- tests/model/test_model_loader.py
+++ tests/model/test_model_loader.py
@@ -1,8 +1,10 @@
 from summarizer.model.model_loader import ModelManager
+from summarizer.model.model_loader import ModelSettings
+import pathlib
 
 
 def test_that_read_config_yields_default_config():
-    from summarizer.model.model_loader import ModelSettings
+
 
     default_settings = ModelSettings().dict()
 
@@ -10,3 +12,20 @@ def test_that_read_config_yields_default_config():
     for k, v in result_cfg.items():
         assert default_settings[k] == v
 
+
+def test_that_loading_from_azureml_registry_is_correctly_configured(mocker):
+    m = ModelManager.instance()
+
+    auth = mocker.patch.object(m, "build_service_principal")
+    workspace = mocker.patch("summarizer.model.model_loader.Workspace", spec=True)
+    model = mocker.patch("summarizer.model.model_loader.Model")
+    model.configure_mock(**{"name.return_value": "my_model"})
+
+    target_dir = pathlib.Path(__file__).parent / "some_cache"
+    cfg = ModelSettings()
+
+    m._fetch_from_azureml_registry(target_dir, cfg)
+
+    auth.assert_called_once()
+    workspace.get.assert_called_once()
+    model.assert_called_once()
