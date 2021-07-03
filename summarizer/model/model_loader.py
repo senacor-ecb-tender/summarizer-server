429450d4642c3c2be7465a32d5d32acd7c6dba83
Kilian Kluge <kilian.kluge@gmail.com>
2021-07-03 18:17:50 +0200

    Refactor model_loader, add test that ensures all config variables are passed

--- summarizer/model/model_loader.py
+++ summarizer/model/model_loader.py
@@ -1,7 +1,6 @@
 import logging
 import os
-from os import path
-from typing import Optional
+import pathlib
 
 import torch
 from azureml.core import Workspace
@@ -69,18 +68,7 @@ class ModelManager:
 
     @staticmethod
     @traced
-    def download_model_from_workspace(workspace: Workspace,
-                                      model_name: str,
-                                      target_dir: str,
-                                      model_version: Optional[int] = None):
-        aml_model = Model(workspace=workspace, name=model_name, version=model_version)
-        model_path = aml_model.download(target_dir=target_dir, exist_ok=True)
-        logger.info(f'Loaded model {aml_model.name} to {model_path}')
-        return model_path
-
-    @staticmethod
-    @traced
-    def read_config() -> dict:
+    def read_config() -> ModelSettings:
         """
         This functions assembles a config dictionary of values required to connect to an Azure ML Workspace. The fct
         attempts to read the config from environment values first, and finally falls back to hard
@@ -89,7 +77,7 @@ class ModelManager:
         :return: a dictionary holding the values for ML model name, Azure subscription, Azure resource group, ML Workspace
         and model version.
         """
-        return ModelSettings().dict()
+        return ModelSettings()
 
     # this can be improved
     # https://docs.microsoft.com/en-us/azure/developer/python/azure-sdk-authenticate
@@ -103,29 +91,40 @@ class ModelManager:
 
     @traced
     def fetch_model(self, cfg: dict = None):
-        auth = self.build_service_principal()
-        download_path = path.join('.', CACHE)
+        download_path = pathlib.Path(CACHE)
+
         if cfg is None:
             cfg = self.read_config()
-        model_version = cfg.get('model_version')
-        target_dir = path.join(download_path, cfg['model_name'], model_version if model_version is not None else 'None')
 
-        downloaded_model_path = path.join(target_dir, cfg.get('model_name'))
-        if not path.exists(downloaded_model_path):
-            ws = Workspace.get(subscription_id=cfg.get('subscription'),
-                               resource_group=cfg.get('resource_group'),
-                               name=cfg.get('ml_workspace'),
-                               auth=auth
-                               )
-            logger.info(f' Loading model from ml registry: {ws.name}')
-            self.download_model_from_workspace(workspace=ws, model_name=cfg.get('model_name'), target_dir=target_dir)
-            logger.info(f' Loaded model from ml registry and stored it: {downloaded_model_path}')
+        version = cfg.model_version if cfg.model_version is not None else 'None'
+
+        target_dir = download_path / cfg.model_name / version
+        downloaded_model_path = target_dir / cfg.model_name
+
+        if not downloaded_model_path.exists() or cfg.model_version is None:
+            logger.info('Loading model from Azure ML registry')
+            self._fetch_from_azureml_registry(target_dir, cfg)
         else:
             logger.info('Loading model from local cache')
 
         self._model = LEDForConditionalGeneration.from_pretrained(downloaded_model_path)
         self._model.to(my_device)
         self._tokenizer = LEDTokenizer.from_pretrained(downloaded_model_path)
+
+    @traced
+    def _fetch_from_azureml_registry(self, target_dir: pathlib.Path, cfg):
+        auth = self.build_service_principal()
+
+        ws = Workspace.get(subscription_id=cfg.subscription,
+                           resource_group=cfg.resource_group,
+                           name=cfg.ml_workspace,
+                           auth=auth
+                           )
+
+        aml_model = Model(workspace=ws, name=cfg.model_name, version=cfg.model_version)
+        model_path = aml_model.download(target_dir=str(target_dir), exist_ok=True)
+
+        logger.info(f'Loaded model {aml_model.name} to {model_path}')
 
 
 model_mgr = ModelManager.instance()
